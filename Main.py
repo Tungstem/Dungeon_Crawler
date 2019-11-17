d0c144d83ce77accf8193fad5ee60ad844615216
Tungstem <57769451+Tungstem@users.noreply.github.com>
2019-11-17 20:37:13 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,18 +1,18 @@
-import Level_Creation
+import Level_Creation_Random
 import On_Screen
 import General_Movement
 
-
-playing=True
+playing = True
 On_Screen.print_title()
-level = Level_Creation.create_grid()
-Level_Creation.initialize_grid(level)
-x, y = Level_Creation.fill_grid(level)
+level, level_row, level_column = Level_Creation_Random.create_grid()
+Level_Creation_Random.initialize_grid(level, level_row, level_column)
+x, y = Level_Creation_Random.fill_grid(level, level_row, level_column)
+
 while playing == True:
-    Level_Creation.draw_grid(level)
+    Level_Creation_Random.draw_grid(level, level_row, level_column)
     choice = On_Screen.show_menu()
     if choice == "1":
-        x, y = General_Movement.hero_movement(x, y, level) #I movimenti sono gestiti direttamente nel modulo on screen
+        x, y = General_Movement.hero_movement(x, y, level, level_row, level_column)  # I movimenti sono gestiti direttamente nel modulo on screen
     if choice == "2":
         On_Screen.item_menu()
     if choice == "3":
@@ -20,13 +20,12 @@ while playing == True:
     if choice == "4":
         On_Screen.skills_menu()
     if choice == "5":
-        playing = False  #In questo modo dico al gioco di fermarsi
+        playing = False  # In questo modo dico al gioco di fermarsi
 
-
-#Cose che mancano:
-# gestione delle entita' player(MC) ed enemy
-# organizzare meglio la creazione della grid di gioco
-# comprendere come usare al meglio pygames
-# i vari menu di gioco
-# un sistema di pathing base per i nemici
-# tutto il sistema di combattimento
+    # Cose che mancano:
+    # gestione delle entita' player(MC) ed enemy
+    # organizzare meglio la creazione della grid di gioco
+    # comprendere come usare al meglio pygames
+    # i vari menu di gioco
+    # un sistema di pathing base per i nemici
+    # tutto il sistema di combattimento
